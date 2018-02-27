9a5f3c54b0982cdf1e8670f11c13fddcea8a30d5
Johan Svensson <johan.svensson93@outlook.com>
2018-02-27 11:28:49 +0100

    Changed API

--- api.js
+++ api.js
@@ -1,5 +1,19 @@
-//key = h1ES5
+var request = new XMLHttpRequest();
 
-let request = new XMLHttpRequest();
-request.open('GET', 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey');
+request.open('GET', 'https://www.forverkliga.se/JavaScript/api/crud.php?key=Xczmv', true);
+
+var myFunc = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(book => {
+      console.log(book.title);
+    });
+  }  else {
+    console.log('error');
+  }
+}
+
 request.send();
