d255630574211dfe6e6646c5564497c3c60e367e
André Topalovic Törnström <tornstromandre@gmail.com>
2018-02-27 15:24:07 +0100

    Added some code in api.js

--- api.js
+++ api.js
@@ -1,19 +1,20 @@
-var request = new XMLHttpRequest();
+var content, txt = "";
 
-request.open('GET', 'https://www.forverkliga.se/JavaScript/api/crud.php?key=Xczmv', true);
-
-var myFunc = function () {
-
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(book => {
-      console.log(book.title);
-    });
-  }  else {
-    console.log('error');
-  }
-}
-
+let request = new XMLHttpRequest();
+request.open('GET', 'https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=tFVZr');
+request.onreadystatechange = function(event) {
+  if(request.status == 200 && request.readyState == 4) {
+    console.log("Respons: " + request.responseText);
+    content = JSON.parse(request.responseText);
+    console.log("Här kommer content: ", content);
+      txt += "<table border='1'>";
+        for (let x in content) {
+          let y = content[x];
+            txt += "<tr><td>" + y[0].title + "</td>";
+            txt += "<td>" + y[0].author + "</td></tr>";
+        }
+      txt += "</table>";
+      document.getElementById('content').innerHTML = txt;
+    }
+  };
 request.send();
